1fc2617ba4cfa47f25888f672fc200df09060e9c
Emmanuel Frécon <efrecon@gmail.com>
2025-04-17 17:17:57 +0200

    Install from image, use regctl for extraction

--- share/features/30-install-node.sh
+++ share/features/30-install-node.sh
@@ -41,8 +41,21 @@ done
 # node-based apps to install. This is a space separated list of apps to install.
 : "${INSTALL_NODE_APPS:="bun typescript ts-node tslint eslint prettier"}"
 
-# Install from source
-: "${INSTALL_NODE_FROM_SOURCE:="auto"}"
+# Where should we install from. This is either "auto" (default), "image" or
+# "source". When "image" compiled binaries will be picked from an existing
+# image.
+: "${INSTALL_NODE_SOURCE:="auto"}"
+
+# Docker registry to use for the image when installing binaries out of an
+# existing image. Possible other values are: mirror.gcr.io or
+# public.ecr.aws/docker
+: "${INSTALL_NODE_REGISTRY:="docker.io"}"
+
+# Name of the node image to use when installing binaries out of an existing
+# image.
+: "${INSTALL_NODE_IMAGE:="library/node"}"
+
+: "${INSTALL_NODE_REGCLIENT_VERSION:="0.8.2"}"
 
 log_init INSTALL
 
@@ -60,11 +73,20 @@ node_version() {
 # relevant.
 install_sidekicks() {
   verbose "Installing yarm and pnpm %s" "$(printf %s\\n "$*" | tr '_' ' ')"
-  # Upgrade and prepare package managers
-  "$@" npm install -g corepack
-  "$@" corepack enable
-  "$@" corepack prepare yarn@stable --activate
-  "$@" corepack prepare pnpm@latest --activate
+  if ! check_command corepack; then
+    # Upgrade and prepare package managers
+    verbose "Installing and enabling corepack"
+    "$@" npm install -g corepack
+    "$@" corepack enable
+  fi
+  if ! check_command yarn; then
+    verbose "Installing yarn"
+    "$@" corepack prepare yarn@stable --activate
+  fi
+  if ! check_command pnpm; then
+    verbose "Installing pnpm"
+    "$@" corepack prepare pnpm@latest --activate
+  fi
 
   # Install the node-based apps
   for app in $INSTALL_NODE_APPS; do
@@ -77,13 +99,8 @@ install_sidekicks() {
   done
 }
 
-# TODO: building node from source takes too long. Try copying from existing image?
-# 1. pull the image with docker image pull
-# 2. save the image with docker save to a local directory.
-# 3. Inside the directory, locate the largest .tar file, e.g. e7534a3f8e08ced32b4ae96362bd638da2ee72ca094e149a4d40ca345c3f0f5a.tar
-# 4. Extract the tar file
-# 5. Copy the content of the usr/local directory into our target prefix.
-
+# Building node from source takes a long time, as in several hours when running
+# in GitHub actions.
 build_from_source() {
   verbose "Downloading and building Node.js %s from source" "$1"
   curdir=$(pwd)
@@ -147,6 +164,67 @@ build_from_source() {
 }
 
 
+# Install regclient https://regclient.org/ in order to be able to extract the
+# content of docker files without the docker client installed (and without the
+# docker daemon running: at installation time, the daemon is not running yet).
+install_regclient() {
+  arch=$(printf %s\\n "$(get_arch)" | sed 's/x64/amd64/g')
+  regclient_url=https://github.com/regclient/regclient/releases/download/v${INSTALL_NODE_REGCLIENT_VERSION}/regctl-$(get_os)-${arch}
+  download "$regclient_url" > "${INSTALL_PREFIX}/bin/regctl"
+  chmod a+x "${INSTALL_PREFIX}/bin/regctl"
+}
+
+
+# Install Node.js from a docker image, this will match the version of node onto
+# an image for the current OS, if possible.
+install_from_image() {
+  # Find out the part of the tag that matches the current OS, if possible.
+  distro=$(get_distro_name)
+  case "$distro" in
+    alpine)
+      # Tags only use major.minor for the version of alpine.
+      os_tag="${distro}$(printf %s\\n "$(get_distro_version)"|grep -Eo '^[0-9]+\.[0-9]+')";;
+    debian)
+      # Pick the slim version for debian, as we just want to copy as little as
+      # possible.
+      os_tag="$(get_release_info VERSION_CODENAME)-slim";;
+    *)
+      # Can we do something about debian-derivatives?
+      error "Unsupported distribution: %s" "$distro";;
+  esac
+  tag="${1}-${os_tag}"
+
+  # install regctl if not already installed
+  check_command "regctl" || install_regclient
+
+  # Extract content of (remote) image to a temporary directory, for the current
+  # platform.
+  img=${INSTALL_NODE_REGISTRY%/}/${INSTALL_NODE_IMAGE}:$tag
+  verbose "Extracting node v%s from image %s" "$1" "$img"
+  imgdir=$(mktemp -d)
+  regctl image export --platform local "$img" | tar -C "$imgdir" -xf -
+
+  # Find the largest tar file in the image, it's where node is. Its name is a 64
+  # character hexadecimal string. The pattern pads 0 with 64 leading zeros and
+  # replaces the 0 with [0-9a-fA-F] to match the tar file name.
+  ptn=$(printf "%0*d" 64 "0"|sed 's/0/[0-9a-fA-F]/g')
+  layer=$(find "$imgdir" -type f -name "$ptn" -exec du -b {} + | sort -n | tail -1 | cut -f2)
+
+  # Extract it to another temp directory
+  layerdir=$(mktemp -d)
+  tar -C "$layerdir" -xf "$layer"
+  chmod -R a+r "$layerdir"
+  rm -rf "$imgdir";  # Micro optimize disk space: remove the image directory
+
+  # Copy the content of the usr/local directory into our target prefix
+  as_root cp -r "$layerdir"/usr/local/* "$INSTALL_PREFIX"
+
+  # Clean up
+  rm -rf "$layerdir"
+  verbose "Installed Node.js %s from image %s" "$1" "$tag"
+}
+
+
 if ! check_command "node" && [ -n "$INSTALL_NODE_VERSION" ]; then
   # Find out where to get the tarball from. This will switch to the unofficial
   # builds when running on musl.
@@ -166,7 +244,7 @@ if ! check_command "node" && [ -n "$INSTALL_NODE_VERSION" ]; then
   latest=$(node_version)
   verbose "Installing Node $latest"
 
-  if [ "$INSTALL_NODE_FROM_SOURCE" = "auto" ]; then
+  if [ "$INSTALL_NODE_SOURCE" = "auto" ]; then
     # When using the unofficial builds, we need to add the libc type to the OS.
     if [ "$INSTALL_NODE_DOMAIN" = "unofficial-builds.nodejs.org" ]; then
       arch=$(get_arch)
@@ -190,13 +268,19 @@ if ! check_command "node" && [ -n "$INSTALL_NODE_VERSION" ]; then
   fi
 
   if [ -z "$INSTALL_TGZURL" ]; then
-    build_from_source "${latest#v}"
+    if [ "$INSTALL_NODE_SOURCE" = "image" ] || [ "$INSTALL_NODE_SOURCE" = "auto" ]; then
+      install_from_image "${latest#v}"
+    elif [ "$INSTALL_NODE_SOURCE" = "source" ]; then
+      build_from_source "${latest#v}"
+    else
+      error "Don't know how to install Node.js %s" "$latest"
+    fi
   else
     verbose "Downloading Node.js from: $INSTALL_TGZURL"
     # TODO: Verify sha256 sums through file SHASUMS256.txt from same URL.
     download "$INSTALL_TGZURL" | as_root tar -C "$INSTALL_PREFIX" -xzf - --strip-components 1 --exclude='*.md' --exclude='LICENSE'
   fi
-  verbose "Installed Node: $(node --version)"
+  verbose "Installed Node.js: $(node --version)"
 
   # Create user settings
   HM=$(home_dir "$INSTALL_USER")
@@ -205,7 +289,7 @@ if ! check_command "node" && [ -n "$INSTALL_NODE_VERSION" ]; then
   ln -sf "${HM}/.config/npmrc" "${HM}/.npmrc"
   as_root chown "$INSTALL_USER" "${HM}/.npmrc" "${HM}/.config/npmrc"
   as_user "$USR" npm config set prefix "${INSTALL_USER_PREFIX}"
-  verbose "npm installations for user %s user under %s, as per %s" \
+  verbose "npm installations for user %s under %s, as per %s" \
     "$USR" "$INSTALL_USER_PREFIX" "${HM}/.npmrc"
 
   # Create system settings
