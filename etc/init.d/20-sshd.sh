4e33d7c9e9e4cfcf7074821b988faf5818d9f4a2
Emmanuel Frécon <efrecon@gmail.com>
2025-04-23 22:27:35 +0200

    Add user section and server section + dirs

--- etc/init.d/20-sshd.sh
+++ etc/init.d/20-sshd.sh
@@ -29,8 +29,6 @@ done
 
 : "${SSHD_USER:=""}"
 
-: "${SSHD_SHELL:=""}"
-
 : "${SSHD_GITHUB_USER:="${TUNNEL_GITHUB_USER:-""}"}"
 
 : "${SSHD_PORT:="${TUNNEL_SSH:-"2222"}"}"
@@ -46,7 +44,7 @@ done
 
 # shellcheck disable=SC2034 # Used from functions in common.sh
 CODE_DESCR="ssh daemon startup"
-while getopts "g:l:p:s:u:vh" opt; do
+while getopts "g:l:p:u:vh" opt; do
   case "$opt" in
     g) # GitHub user to fetch keys from
       SSHD_GITHUB_USER="$OPTARG";;
@@ -54,8 +52,6 @@ while getopts "g:l:p:s:u:vh" opt; do
       SSHD_PORT="$OPTARG";;
     l) # Where to send logs
       SSHD_LOG="$OPTARG";;
-    s) # Shell to use
-      SSHD_SHELL="$OPTARG";;
     u) # User to accept at ssh daemon
       SSHD_USER="$OPTARG";;
     v) # Increase verbosity, repeat to increase
@@ -71,27 +67,40 @@ done
 
 log_init SSHD
 
+
+make_owned_dir() {
+  as_root mkdir -p "$1"
+  as_root chown "$2" "$1"
+  as_root chmod go-rwx "$1"
+}
+
+
 configure_sshd() {
   as_root mkdir -p "${SSHD_CONFIG_DIR}"
-  as_root chmod go-rwx "${SSHD_CONFIG_DIR}"
+  make_owned_dir "${SSHD_CONFIG_DIR}/user" "$SSHD_USER"
+  make_owned_dir "${SSHD_CONFIG_DIR}/server" "root"
 
   as_root mkdir -p "${SSHD_PREFIX}/log"
 
   if [ -n "$SSHD_GITHUB_USER" ]; then
     verbose "Collecting public keys from %s" "$SSHD_GITHUB_USER"
-    download "https://github.com/${SSHD_GITHUB_USER}.keys" - | as_root tee "${SSHD_CONFIG_DIR}/authorized_keys" > /dev/null
-    as_root chmod go-rwx "${SSHD_CONFIG_DIR}/authorized_keys"
+    download "https://github.com/${SSHD_GITHUB_USER}.keys" - > "${SSHD_CONFIG_DIR}/user/authorized_keys"
+    chmod go-rwx "${SSHD_CONFIG_DIR}/user/authorized_keys"
   fi
 
   verbose "Generating ssh host keys"
-  as_root ssh-keygen -q -f "${SSHD_CONFIG_DIR}/ssh_host_rsa_key" -N '' -b 4096 -t rsa
+  as_root ssh-keygen -q -f "${SSHD_CONFIG_DIR}/server/ssh_host_rsa_key" -N '' -b 4096 -t rsa
+  as_root cp "${SSHD_CONFIG_DIR}/server/ssh_host_rsa_key.pub" "${SSHD_PREFIX}/etc/ssh_host_rsa_key.pub"
+
+  verbose "Making ssh host public key at %s readable by %s" "${SSHD_PREFIX}/etc/ssh_host_rsa_key.pub" "$SSHD_USER"
+  as_root chown "$SSHD_USER" "${SSHD_PREFIX}/etc/ssh_host_rsa_key.pub"
 
   SSHD_TEMPLATE=$(mktemp)
   cat <<'EOF' > "$SSHD_TEMPLATE"
 LogLevel DEBUG3
 Port $PORT
-HostKey $PWD/ssh_host_rsa_key
-PidFile $PWD/sshd.pid
+HostKey $PWD/server/ssh_host_rsa_key
+PidFile $PWD/server/sshd.pid
 
 # PAM is necessary for password authentication on Debian-based systems
 UsePAM yes
@@ -108,15 +117,13 @@ PasswordAuthentication no
 # Only allow single user
 AllowUsers $USER
 
-# Only allow those keys
-AuthorizedKeysFile $PWD/authorized_keys
-
 # Turns on sftp-server
 Subsystem    sftp    /usr/lib/ssh/sftp-server
 
 # Force the shell for the user
 Match User $USER
-SetEnv SHELL=$SHELL
+  # Only allow those keys
+  AuthorizedKeysFile $PWD/user/authorized_keys
 EOF
 
   if [ -f "$SSHD_TEMPLATE" ]; then
@@ -124,7 +131,6 @@ EOF
       -e "s,\$PWD,${SSHD_CONFIG_DIR},g" \
       -e "s,\$USER,${SSHD_USER},g" \
       -e "s,\$PORT,${SSHD_PORT},g" \
-      -e "s,\$SHELL,${SSHD_SHELL},g" \
       "$SSHD_TEMPLATE" | as_root tee "${SSHD_CONFIG_DIR}/sshd_config" > /dev/null
     as_root chmod go-rwx "${SSHD_CONFIG_DIR}/sshd_config"
     rm -f "$SSHD_TEMPLATE"
@@ -139,10 +145,6 @@ fi
 if [ -z "$SSHD_USER" ]; then
   SSHD_USER=$(id -un)
   verbose "Restricting sshd to user %s" "$SSHD_USER"
-fi
-if [ -z "$SSHD_SHELL" ]; then
-  SSHD_SHELL=$(getent passwd "$SSHD_USER" | cut -d: -f7)
-  verbose "Using shell %s for %s in sshd" "$SSHD_SHELL" "$SSHD_USER"
 fi
 
 # If we are to daemonize, do it now and exit. Export all our variables to the
