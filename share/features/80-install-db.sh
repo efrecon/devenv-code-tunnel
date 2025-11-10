69da8afa5c5fe740b62b4335aaaef0bd75d363a4
Emmanuel Frécon <emmanuel.frecon@gpsgate.com>
2025-11-10 15:40:48 +0100

    ✨ Add support for DB clients

---
+++ share/features/80-install-db.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Shell sanity. Stop on errors, undefined variables and pipeline errors.
+# shellcheck disable=SC3040 # ok, see: https://unix.stackexchange.com/a/654932
+set -euo pipefail
+
+# Absolute location of the script where this script is located.
+INSTALL_ROOTDIR=$( cd -P -- "$(dirname -- "$(command -v -- "$(realpath "$0")")")" && pwd -P )
+
+# Hurry up and find the libraries
+for lib in log common install system; do
+  for d in ../../lib ../lib lib; do
+    if [ -d "${INSTALL_ROOTDIR}/$d" ]; then
+      # shellcheck disable=SC1090
+      . "${INSTALL_ROOTDIR}/$d/${lib}.sh"
+      break
+    fi
+  done
+done
+
+
+# All following vars have defaults here, but will be set and inherited from
+# calling install.sh script in the normal case.
+: "${INSTALL_VERBOSE:=0}"
+: "${INSTALL_LOG:=2}"
+
+
+log_init INSTALL
+
+
+if is_os_family alpine; then
+  install_packages mysql-client postgresql17-client sqlite redis
+elif is_os_family debian; then
+  install_packages mysql-client postgresql-client-17 sqlite3 redis-server redis-tools
+else
+  error "Unsupported OS family: %s" "$(get_distro_name)"
+fi
