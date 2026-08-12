74d720de1f76b6d1ad07510e85e24bfb37af5c17
Emmanuel Frécon <efrecon@gmail.com>
2026-08-12 15:57:55 +0200

    ✨ Install chisel

---
+++ share/features/40-install-remote.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Shell sanity. Stop on errors, undefined variables and pipeline errors.
+# shellcheck disable=SC3040 # ok, see: https://unix.stackexchange.com/a/654932
+set -euo pipefail
+
+# Absolute location of the script where this script is located.
+INSTALL_ROOTDIR=$( cd -P -- "$(dirname -- "$(command -v -- "$(realpath "$0")")")" && pwd -P )
+
+# Hurry up and find the libraries
+for lib in log common install system; do
+  for d in ../../lib ../lib lib; do
+    if [ -d "${INSTALL_ROOTDIR}/$d" ]; then
+      # shellcheck disable=SC1090
+      . "${INSTALL_ROOTDIR}/$d/${lib}.sh"
+      break
+    fi
+  done
+done
+
+
+# All following vars have defaults here, but will be set and inherited from
+# calling install.sh script in the normal case.
+: "${INSTALL_VERBOSE:=0}"
+: "${INSTALL_LOG:=2}"
+: "${INSTALL_USER:="coder"}"
+: "${INSTALL_PREFIX:="/usr/local"}"
+: "${INSTALL_USER_PREFIX:="${HOME}/.local"}"
+: "${INSTALL_TARGET:="user"}"
+
+: "${INSTALL_CHISEL_VERSION:="1.11.8"}"
+
+# URL to download the code CLI from.
+: "${INSTALL_CHISEL_URL:="https://github.com/jpillora/chisel/releases/download/v${INSTALL_CHISEL_VERSION}/chisel_${INSTALL_CHISEL_VERSION}_$(get_os)_$(get_golang_arch).gz"}"
+: "${INSTALL_CHISEL_SUMS:="https://github.com/jpillora/chisel/releases/download/v${INSTALL_CHISEL_VERSION}/chisel_${INSTALL_CHISEL_VERSION}_checksums.txt"}"
+
+
+log_init INSTALL
+
+
+debug "Installing chisel v%s" "$INSTALL_CHISEL_VERSION"
+
+# Install the chisel CLI in the proper directory location, i.e. as per
+# INSTALL_TARGET preference.
+[ "$INSTALL_TARGET" = "user" ] \
+  && BINDIR="${INSTALL_USER_PREFIX}/bin" \
+  || BINDIR="${INSTALL_PREFIX}/bin"
+chisel=$(internet_bin_installer \
+                "$INSTALL_CHISEL_URL" \
+                "$BINDIR" \
+                "chisel" \
+                "$INSTALL_CHISEL_SUMS")
+
+# Verify installation through printing the version.
+verbose "Installed chisel %s" "$("$chisel" --version)"
