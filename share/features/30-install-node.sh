77424de558cebbcc1a626b1cdcc646fe7ad1550a
Emmanuel Frecon <9443924+efrecon@users.noreply.github.com>
2026-09-18 13:33:25 +0200

    ⚡️ Use pre-built binaries for node>=22 on musl/arm64 (#96)
    * ⚡️ Download musl ready-made binaries for node>=22
    * ⬆️ Upgrade docker install script
    * 🐛 Remove leading v when present
    * 🐛 Revamp logic to match what's available

--- share/features/30-install-node.sh
+++ share/features/30-install-node.sh
@@ -295,6 +295,8 @@ if ! command_present "node" && [ -n "$INSTALL_NODE_VERSION" ]; then
   if [ -z "$latest" ]; then
     error "No latest version of Node.js matching %s found" "$INSTALL_NODE_VERSION"
   fi
+  _node_major=${latest#v}
+  _node_major=${_node_major%%.*}
   debug "Installing Node %s" "$latest"
 
   if [ "$INSTALL_NODE_SOURCE" = "auto" ]; then
@@ -303,9 +305,15 @@ if ! command_present "node" && [ -n "$INSTALL_NODE_VERSION" ]; then
     if [ "$INSTALL_NODE_DOMAIN" = "unofficial-builds.nodejs.org" ]; then
       debug "Installing Node.js %s for %s %s" "$latest" "$(get_os)" "$arch"
       if is_musl_os; then
-        # musl builds are only available for x64
         if [ "$arch" = "x64" ]; then
           INSTALL_TGZURL="${INSTALL_ROOTURL}/${latest}/node-${latest}-$(get_os)-${arch}-musl.tar.gz"
+        elif [ "$arch" = "arm64" ]; then
+          if [ "$_node_major" -ge 22 ]; then
+            INSTALL_TGZURL="${INSTALL_ROOTURL}/${latest}/node-${latest}-$(get_os)-${arch}-musl.tar.gz"
+          else
+            debug "No binaries available for %s and musl, will build from source" "$arch"
+            INSTALL_TGZURL=
+          fi
         else
           debug "No binaries available for %s and musl, will build from source" "$arch"
           INSTALL_TGZURL=
